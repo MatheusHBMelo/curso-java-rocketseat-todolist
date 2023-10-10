b853505dae2d6837939c9e1d81e0039d1d4f0758
MatheusHBMelo <matheushbmelo@gmail.com>
2023-10-10 09:45:53 -0300

    Atualizada a entidade de usuarios para o modelo JPA e Lombok

--- src/main/java/br/com/matheushbmelo/todolist/models/UserModel.java
+++ src/main/java/br/com/matheushbmelo/todolist/models/UserModel.java
@@ -1,44 +1,33 @@
 package br.com.matheushbmelo.todolist.models;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "TB_USER")
+@Data
 public class UserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
 
-    public UserModel() {
-    }
-
-    public UserModel(String username, String name, String password) {
-        this.username = username;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
